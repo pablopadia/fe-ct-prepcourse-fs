7f1f8e28680220eaafd884ec81f99cc7082194f2
pablopadia <pdpadia@gmail.com>
2023-09-08 11:20:47 -0300

    commit m03 completado

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,39 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return (x+y);
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return (x-y);
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   if (y!=0) {
+      return (x/y);
+   } else {
+      return (null);
+   }
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return (x*y);
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   if (y!=0) {
+      return (x%y);
+   } else {
+      return (null);
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
